a434a805859472a1abe0c286386b7611c1c7d14c
Avalyn Project <140300971+avalyn-project@users.noreply.github.com>
2023-09-08 03:38:52 -0500

    Create user.js

---
+++ commands/utility/user.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, GatewayIntentBits, Client, Events } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Provides information about the user.'),
+	async execute(interaction) {
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		await interaction.reply({ content: `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`, ephemeral: true });
+	},
+};
