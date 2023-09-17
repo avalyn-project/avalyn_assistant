7158bd4734a96f1c851966bc4adb772b0b671e04
Avalyn Project <140300971+avalyn-project@users.noreply.github.com>
2023-09-17 08:13:33 -0500

    09/17/2023 - Combined init.js and cmds.js

--- init.js
+++ init.js
@@ -1,6 +1,18 @@
 
 // *********************************************************************************************************** //
-// Copytight (c) 2023 The Avalyn Project                                                                       //
+// Welcome to the Avalyn Discord assistant, what you can do with this application is simple, you can           //
+// easily moderate your discord servers.                                                                       //
+//                                                                                                             //
+// But, the difference is, The Avalyn Project, is also embedded within, allowing you to use                    //
+// 'The Avalyn Project' to create your own Blockchain and Blockchain tools!                                    //
+//                                                                                                             //
+// <Q:> How is that possible?                                                                                  //
+// <A:> Well, with the included plugins created by                                                             //
+//      The Avalyn Project (c) 2023.                                                                           //
+//                                                                                                             //
+//                                                                                                             //
+//                                                                                                             //
+//                                                                                                             //
 // *********************************************************************************************************** //
 
 const fs = require('node:fs');
@@ -38,10 +50,6 @@ client.once(Events.ClientReady, c => {
 // The initial connection to the GUI Server and Port, you can use the official servers that are operated by   //
 // The Avalyn Project.                                                                                        //
 //                                                                                                            //
-// Server [UI]: getavalyn.org                                                                                 //
-// Server [RPC]: rpc-client.getavalyn.org                                                                     //
-// Port [1]: 1280                                                                                             //
-// Port [2]: 1281                                                                                             //
 // ********************************************************************************************************** //
 require("./lib/core/http/server.js");
 require("./lib/core/rpc/client.js");
@@ -51,7 +59,7 @@ client.user.setStatus(`idle`);
 client.user.setActivity({
     name: "🖥️",
     type: ActivityType.Streaming,
-    url: "https://twitch.tv/avalyn_js",
+    url: "https://twitch.tv/avalyn_project",
 });
 
 });
@@ -76,5 +84,48 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-require('./cmds.js');
+console.clear()
+const { REST, Routes } = require('discord.js');
+
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(clc.white(` Welcome to the Avalyn Discord assistant.\n\n [`) + clc.cyan(`On The Web`) + clc.white(`]: `) + clc.green(`https://`) + clc.white(`getavalyn.org/\n [`) + clc.cyan(`About Avalyn`) + clc.white(`]: `) + clc.green(`https://`) + clc.white(`getavalyn.org/about\n`) + clc.green(`\n ---------------------------------------------------------------\n`) + clc.white(` Started refreshing `) + clc.green(`${commands.length} `) + clc.white(`application (`) + clc.green(`/`) + clc.white(`) commands`) + clc.green(`.`));
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(clc.white(` Successfully refreshed `) + clc.green(`${data.length} `) + clc.white(`application (`) + clc.green(`/`) + clc.white(`) commands`) + clc.green(`.\n`));
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
 client.login(token);
